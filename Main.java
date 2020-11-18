dc214c528584090f49db9e13d1626905d41a4740
shynn <shynn.lawrence@gmail.com>
2020-11-17 19:57:53 -0800

    Created Swing basics

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 import javax.swing.*;
 
+class GUI{
+    public void GUI(){
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);
+    }
+}
+
+
 class Main{
     public static void main(String[] args){
         System.out.println("Testing.");
