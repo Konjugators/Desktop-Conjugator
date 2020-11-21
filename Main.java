f9586d043032d2e52541df1071655c812375063c
shynn <shynn.lawrence@gmail.com>
2020-11-21 10:10:38 -0800

    Deleted unnecessary files

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import desktop.conjugator;
 import desktop.verb;
@@ -7,10 +9,35 @@ import desktop.verbFind;
 
 class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        String arg1 = args[0];
+        String arg2 = args[1];
+        String arg3 = args[2];
         verbFind.verbDict();
-        ArrayList<String> temp = verbFind.getVerbInterfaceMap().get("putzen").getPastConjugations();
+        verb temp = verbFind.getVerbInterfaceMap().get(arg1);
+        HashMap<String, Method> test = new HashMap<String, Method>();
+        ArrayList<String> Conjugations = new ArrayList<String>();
+        switch (arg3) {
+            case ("present"):
+                Conjugations = temp.getPresentConjugations();
+                break;
+            case ("past"):
+                Conjugations = temp.getPastConjugations();
+                break;
+            case ("perfect"):
+                Conjugations = temp.getPerfectConjugations();
+                break;
+            case ("past-perfect"):
+                Conjugations = temp.getPastPerfectConjugations();
+                break;
+            case ("future"):
+                Conjugations = temp.getFutureConjugations();
+                break;
+            default:
+                Conjugations = temp.getPresentConjugations();
+                break;
+        }
         conjugator.conjugatorSetup();
-        System.out.println(conjugator.pronounConjugate(temp, "er"));
+        System.out.println(conjugator.pronounConjugate(Conjugations, arg2));
     }
 }
 
