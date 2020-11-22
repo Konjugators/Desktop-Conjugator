fc21b29cbdb6de19ba7ebb7421d83819ba6a4436
shynn <shynn.lawrence@gmail.com>
2020-11-21 18:08:57 -0800

    Fundamentals setup

--- desktop/GUI.java
+++ desktop/GUI.java
@@ -1,5 +1,8 @@
 package desktop;
+
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -13,33 +16,25 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
-// // import jdk.internal.org.jline.terminal.MouseEvent.Button;
-// class ButtonListener implements ActionListener {
+class ButtonListener implements ActionListener {
+    ButtonListener() {
+    }
 
-//     ButtonListener() {
-//     }
-
-//     // public void actionPerformed(ActionEvent e) {
-//     //     if (e.getActionCommand().equals("Button1")) {
-//     //         Main.nameLabel.setText(Main.infinitiveField.getText());
-//     //     }
-//     // }
-// }
-
-class Initialization{
-
-}
+    public void actionPerformed(ActionEvent e) {
+      if (e.getActionCommand().equals("Conjugate!")) {
+      }
+    }
+  }
 
 public class GUI extends JPanel{
     static GridBagConstraints gbc;
     static Container container;
-    ButtonGroup tenseGroup = new ButtonGroup();
-    ButtonGroup pronounGroup = new ButtonGroup();
-    ButtonGroup conjugations;
+    static ButtonGroup tenseGroup = new ButtonGroup();
+    static ButtonGroup pronounGroup = new ButtonGroup();
 
     public void parameterSetup(){
         // Initializations
-        GridBagConstraints gbctemp  = new GridBagConstraints();
+        GridBagConstraints gbctemp1  = new GridBagConstraints();
         JRadioButton temp;
         JLabel tensesLabel = new JLabel();
         JLabel pronounsLabel = new JLabel();
@@ -49,42 +44,55 @@ public class GUI extends JPanel{
         String[] pronouns = new String[]{"ich", "du  ", "er  ", "wir", "ihr", "sie"};
 
         // Tense Label
-        gbctemp.gridx = -3;
-        gbctemp.gridy = -1;
-        container.add(tensesLabel, gbctemp);
+        gbctemp1.gridx = -6;
+        gbctemp1.gridy = -1;
+        container.add(tensesLabel, gbctemp1);
 
         // Tense buttons starter grid
-        gbctemp.gridx = -3;
-        gbctemp.gridy= 0;
-        
+        gbctemp1.gridx = -6;
+        gbctemp1.gridy= 0;
+
         for (String z : tenses){
             temp = new JRadioButton(z);
             temp.setActionCommand(z);
             tenseGroup.add(temp);
-            gbctemp.gridy += 1;
-            container.add(temp, gbctemp);
+            gbctemp1.gridy += 1;
+            container.add(temp, gbctemp1);
         }
-        
+        verbInputSetup();
+        GridBagConstraints gbctemp2  = new GridBagConstraints();
         // Pronouns Label
-        gbctemp.gridx = 3;
-        gbctemp.gridy = -1;
-        container.add(pronounsLabel, gbctemp);
+        gbctemp2.gridx = 11;
+        gbctemp2.gridy = -1;
+        container.add(pronounsLabel, gbctemp2);
 
         // Pronoun buttons
-        gbctemp.gridx = 3;
-        gbctemp.gridy= 0;
+        gbctemp2.gridx = 11;
+        gbctemp2.gridy= 0;
 
         for (String z : pronouns){
             temp = new JRadioButton(z);
             temp.setActionCommand(z);
             pronounGroup.add(temp);
-            gbctemp.gridy += 1;
-            container.add(temp, gbctemp);
+            gbctemp2.gridy += 1;
+            container.add(temp, gbctemp2);
         }
     }
 
+    public void verbInputSetup(){
+
+        JLabel instructions = new JLabel();
+        instructions.setText("Type the infinitive:");
+        gbc.gridx = 5;
+        gbc.gridy = 0;
+        container.add(instructions, gbc);
+
+    }
+
     public GUI(){
+        gbc = new GridBagConstraints();
         parameterSetup();
+        // verbInputSetup();
     }
 
 
@@ -92,9 +100,6 @@ public class GUI extends JPanel{
     public static void main(String[] args) {
         // GUI gui = new GUI();
         JFrame frame = new JFrame();
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 0;
         container = frame.getContentPane();
         container.setLayout(new GridBagLayout());
         container.add(new GUI());
