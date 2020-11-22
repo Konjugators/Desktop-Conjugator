d7cda374a9f31d386bb82efbce7b266845d4a204
shynn <shynn.lawrence@gmail.com>
2020-11-21 21:18:50 -0800

    Finished GUI

--- desktop/GUI.java
+++ desktop/GUI.java
@@ -1,13 +1,13 @@
 package desktop;
 
 import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Enumeration;
 
+import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,23 +17,75 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 class ButtonListener implements ActionListener {
+    public String clickedTense;
+    public int clickedPronoun;
     ButtonListener() {
     }
 
+    public static String getTenseClicked(){
+        for (Enumeration<AbstractButton> buttons = GUI.tenseGroup.getElements(); buttons.hasMoreElements();) {
+            AbstractButton button = buttons.nextElement();
+            if (button.isSelected()) {
+                return button.getText().strip();
+            }
+        }
+        return "";
+    }
+
+    public static String getPronounClicked(){
+        for (Enumeration<AbstractButton> buttons = GUI.pronounGroup.getElements(); buttons.hasMoreElements();) {
+            AbstractButton button = buttons.nextElement();
+            if (button.isSelected()) {
+                return button.getText().strip();
+            }
+        }
+        return "";
+    }
+
+    public static void changeGBCGrid(int x, int y){
+        GUI.gbc.gridx = x;
+        GUI.gbc.gridy = y;
+    }
+
     public void actionPerformed(ActionEvent e) {
-      if (e.getActionCommand().equals("Conjugate!")) {
-          System.out.println("Conjugated!");
-      }
+        if (e.getActionCommand().equals("Conjugate!")) {
+            GUI.conjugatedVerb.setText("verb is "+GUI.infinitiveField.getText());
+            GUI.clickedAmount += 1;
+        }
+
+        if (GUI.clickedAmount >= 1){
+            // Placeholder Text
+            // if (GUI.clickedAmount >= 2){
+            //     GUI.container.remove(GUI.conjugatedLabel);
+            // }
+            changeGBCGrid(1, 7);
+            GUI.gbc.ipadx = 0;
+            GUI.gbc.ipady = 0;
+            
+            GUI.conjugatedLabel.setText(GUI.infinitiveField.getText());
+            if (GUI.clickedAmount == 1){
+                GUI.container.add(GUI.conjugatedLabel, GUI.gbc);
+            }
+            // GUI.conjugatedLabel.setText("");
+        }
     }
   }
 
 public class GUI extends JPanel{
-    static GridBagLayout gbc;
-    static Container container;
-    static ButtonGroup tenseGroup = new ButtonGroup();
-    static ButtonGroup pronounGroup = new ButtonGroup();
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    public static GridBagLayout gbl = new GridBagLayout();
+    public static GridBagConstraints gbc = new GridBagConstraints();
+    public static Container container;
+    public static ButtonGroup tenseGroup = new ButtonGroup();
+    public static ButtonGroup pronounGroup = new ButtonGroup();
     public static JTextField infinitiveField;
     public static JButton submitButton;
+    public static JLabel conjugatedVerb;
+    public static Integer clickedAmount = 0;
+    public static JLabel conjugatedLabel = new JLabel();
 
     public void parameterSetup(){
         // Initializations
@@ -62,10 +114,11 @@ public class GUI extends JPanel{
 
         for (String z : tenses){
             temp = new JRadioButton(z);
+            temp.setSelected(z.equals("present        "));
             temp.setActionCommand(z);
             tenseGroup.add(temp);
             gbctemp1.gridy += 1;
-            gbc.setConstraints(temp, gbctemp1);
+            gbl.setConstraints(temp, gbctemp1);
             container.add(temp, gbctemp1);
         }
 
@@ -86,9 +139,10 @@ public class GUI extends JPanel{
         for (String z : pronouns){
             temp = new JRadioButton(z);
             temp.setActionCommand(z);
+            temp.setSelected(z.equals("ich"));
             pronounGroup.add(temp);
             gbctemp2.gridy += 1;
-            gbc.setConstraints(temp, gbctemp2);
+            gbl.setConstraints(temp, gbctemp2);
             container.add(temp, gbctemp2);
         }
     }
@@ -114,9 +168,14 @@ public class GUI extends JPanel{
         gbctemp1.gridy = 3;
         submitButton = new JButton("Conjugate!");
         submitButton.addActionListener(new ButtonListener());
-        // submitButton.setSize(30, 30);
         container.add(submitButton, gbctemp1);
-        
+
+        // Conjugated Verb placement
+        gbctemp1.gridx = 2;
+        gbctemp1.gridy = 4;
+        conjugatedVerb = new JLabel();
+        conjugatedVerb.setText("");
+        container.add(conjugatedVerb, gbctemp1);
     }
 
     public GUI(){
@@ -127,11 +186,10 @@ public class GUI extends JPanel{
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         container = frame.getContentPane();
-        gbc = new GridBagLayout();
-        container.setLayout(gbc);
+        container.setLayout(gbl);
         container.add(new GUI());
         frame.pack();
-        frame.setSize(400, 400);
+        frame.setSize(500, 500);
         frame.setVisible(true);
     }
 }
